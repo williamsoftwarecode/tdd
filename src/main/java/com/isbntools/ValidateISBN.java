45590a91cb6420251c66ca8e21deaa3289a2c184
William <williamsoftwarecode@gmail.com>
2022-12-28 14:03:24 +0800

    Refactor - Extract methods for readability

--- src/main/java/com/isbntools/ValidateISBN.java
+++ src/main/java/com/isbntools/ValidateISBN.java
@@ -2,30 +2,43 @@ package com.isbntools;
 
 public class ValidateISBN {
 
+    private static final int SHORT_ISBN_LENGTH = 10;
+    private static final int LONG_ISBN_LENGTH = 13;
+    private static final int SHORT_ISBN_MULTIPLIER = 11;
+    private static final int LONG_ISBN_MULTIPLIER = 10;
+
     public boolean checkISBN(String isbn) {
         int length = isbn.length();
-        if (length != 10 && length != 13) {
+        if (length != SHORT_ISBN_LENGTH && length != LONG_ISBN_LENGTH) {
             throw new NumberFormatException("ISBN numbers must be 10 or 13 digits long");
         }
 
         if (length == 10) {
-            int total = 0;
-            for (int i = 0; i < 10; i++) {
-                char currentChar = isbn.charAt(i);
-                if (!Character.isDigit(currentChar)) {
-                    if (!(i == 9 && currentChar == 'X')) {
-                        throw new NumberFormatException("ISBN numbers can only contain numeric digits");
-                    }
-                    currentChar = 10;
-                }
-                total += Character.getNumericValue(currentChar) * (10 - i);
-            }
+            return isValidISBN10Number(isbn);
+        } else {
+            return isValidISBN13Number(isbn);
+        }
+    }
 
-            return total % 11 == 0;
+    private static boolean isValidISBN10Number(String isbn) {
+        int total = 0;
+        for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
+            char currentChar = isbn.charAt(i);
+            if (!Character.isDigit(currentChar)) {
+                if (!(i == 9 && currentChar == 'X')) {
+                    throw new NumberFormatException("ISBN numbers can only contain numeric digits");
+                }
+                currentChar = 10;
+            }
+            total += Character.getNumericValue(currentChar) * (SHORT_ISBN_LENGTH - i);
         }
 
+        return total % SHORT_ISBN_MULTIPLIER == 0;
+    }
+
+    private static boolean isValidISBN13Number(String isbn) {
         int total = 0;
-        for (int i = 0; i < 13; i++) {
+        for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
             char currentChar = isbn.charAt(i);
             if (!Character.isDigit(currentChar)) {
                 if (!(i == 12 && currentChar == 'X')) {
@@ -34,10 +47,14 @@ public class ValidateISBN {
                 currentChar = 10;
             }
 
-            int multiple = (i % 2 == 0) ? 1 : 3;
+            int multiple = isEvenNumber(i) ? 1 : 3;
             total += Character.getNumericValue(currentChar) * multiple;
         }
 
-        return total % 10 == 0;
+        return total % LONG_ISBN_MULTIPLIER == 0;
+    }
+
+    private static boolean isEvenNumber(int num) {
+        return num % 2 == 0;
     }
 }

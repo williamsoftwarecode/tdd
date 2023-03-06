50d36e35d72b7be71d068e2a62ed294fc3864dbe
William <williamsoftwarecode@gmail.com>
2023-03-06 14:31:29 +0800

    Add mocks for database service and web service for testing behaviour

--- src/test/java/com/isbntools/StockManagementTests.java
+++ src/test/java/com/isbntools/StockManagementTests.java
@@ -3,6 +3,7 @@ package com.isbntools;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class StockManagementTests {
 
@@ -19,5 +20,40 @@ public class StockManagementTests {
         String isbn = "0140177396";
         String locatorCode = stockManager.getLocatorCode(isbn);
         assertEquals("7396J4", locatorCode);
+    }
+
+    @Test
+    void databaseIsUsedIfDataIsPresent() {
+        ExternalISBNDataService mockWebService = mock(ExternalISBNDataService.class);
+        ExternalISBNDataService mockDatabaseService = mock(ExternalISBNDataService.class);
+
+        String isbn = "0140177396";
+        when(mockDatabaseService.lookup(isbn)).thenReturn(new Book(isbn, "a", "a"));
+
+        StockManager stockManager = new StockManager();
+        stockManager.setWebService(mockWebService);
+        stockManager.setDatabaseService(mockDatabaseService);
+
+        stockManager.getLocatorCode(isbn);
+        verify(mockDatabaseService, times(1)).lookup(isbn);
+        verify(mockWebService, times(0)).lookup(anyString());
+    }
+
+    @Test
+    void webServiceIsUsedIfDataIsNotPresentInDatabase() {
+        ExternalISBNDataService mockWebService = mock(ExternalISBNDataService.class);
+        ExternalISBNDataService mockDatabaseService = mock(ExternalISBNDataService.class);
+
+        String isbn = "0140177396";
+        when(mockDatabaseService.lookup(isbn)).thenReturn(null);
+        when(mockWebService.lookup(isbn)).thenReturn(new Book(isbn, "a", "a"));
+
+        StockManager stockManager = new StockManager();
+        stockManager.setWebService(mockWebService);
+        stockManager.setDatabaseService(mockDatabaseService);
+
+        stockManager.getLocatorCode(isbn);
+        verify(mockDatabaseService, times(1)).lookup(isbn);
+        verify(mockWebService, times(1)).lookup(isbn);
     }
 }
